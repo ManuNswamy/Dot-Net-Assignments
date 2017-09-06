a80bdd8d5e481214bb6e47020e4401dab622dea9
ManuNswamy <subramani.nswamy@gmail.com>
2017-09-06 16:58:16 +0530

    Assignments till 6th September

---
+++ GlobalizationLocalization/GlobalizationLocalization/Helper/CultureHelper.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.SessionState;
+
+namespace GlobalizationLocalization.Helper
+{
+    public class CultureHelper
+    {
+        protected HttpSessionState session;
+
+        //Constructor
+        public CultureHelper(HttpSessionState httpSessionState)
+        {
+            session = httpSessionState;
+        }
+
+        //properties
+        public static int CurrentCulture
+        {
+            get
+            {
+                if(Thread.CurrentThread.CurrentUICulture.Name=="en-GB")
+                {
+                    return 0;
+                }
+                else if (Thread.CurrentThread.CurrentUICulture.Name == "hi-IN")
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 2;
+                }
+              
+            }
+            set
+            {
+                if(value == 0)
+                {
+                    Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-GB");
+                }
+                else if (value == 1)
+                {
+                    Thread.CurrentThread.CurrentUICulture = new CultureInfo("hi-IN");
+                }
+                else
+                {
+                    Thread.CurrentThread.CurrentUICulture = CultureInfo.InvariantCulture;
+                }
+
+                Thread.CurrentThread.CurrentCulture = Thread.CurrentThread.CurrentUICulture;
+            }
+        }
+
+    }
+}
---
+++ GlobalizationLocalization/GlobalizationLocalization/Models/UserDetails.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using GlobalizationLocalization.LocalResources;
+
+namespace GlobalizationLocalization.Models
+{
+    public class UserDetails
+    {
+        [Display(Name = "Username", ResourceType = typeof(Resource))]
+        public string Username { get; set; }
+
+        [Display(Name = "Firstname", ResourceType = typeof(Resource))]
+        public string Firstname { get; set; }
+
+        [Display(Name = "Surname", ResourceType = typeof(Resource))]
+        public string Surname { get; set; }
+    }
+}
